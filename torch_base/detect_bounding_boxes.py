2abcf75788d140a0ad9db5a2202d03d16f54463c
Karthik-Ragunath <a.karthikragunath@gmail.com>
2022-08-07 19:59:33 -0600

    debugging

--- torch_base/detect_bounding_boxes.py
+++ torch_base/detect_bounding_boxes.py
@@ -27,6 +27,7 @@ CLASSES = [
 detection_threshold = 0.8
 
 __all__ = ['DetectBoundingBox']
+directory_path = "/media/karthikragunath/Personal-Data/carla_6/RL_CARLA/carla_rgb_sensor_flow_detected"
 
 class DetectBoundingBox:
     def __init__(self, image, image_name):
@@ -35,12 +36,17 @@ class DetectBoundingBox:
         print("%"*50, "Image Name:", self.image_name, "%"*50)
 
     def detect_bounding_boxes(self):
+        '''
+        print('#' * 25, "Inside Detect Bounding Boxes", '#' * 25)
         orig_image = np.copy(self.image)
         self.image = self.image / 255.0
         # bring color channels to front
         image = np.transpose(self.image, (2, 0, 1)).astype(np.float64)
         # convert to tensor
         image = torch.tensor(image, dtype=torch.float)
+
+        print("Image Dimension:", image.shape)
+
         image = image.to(device)
         # add batch dimension
         image = torch.unsqueeze(image, 0)
@@ -67,5 +73,54 @@ class DetectBoundingBox:
                               (0, 0, 255), 2)
             plt.imshow(orig_image)
             plt.savefig("bounding_box_outputs/" + self.image_name)
+
         print("Image Detection Done...", self.image_name)
         print('-' * 50)
+        '''
+
+        print("Image Name (Self):", self.image_name)
+        image = cv2.imread(directory_path + "/" + self.image_name + ".png")
+        orig_image = image.copy()
+        # BGR to RGB
+        image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB).astype(np.float32)
+        # make the pixel range between 0 and 1
+        image /= 255.0
+        # bring color channels to front
+        image = np.transpose(image, (2, 0, 1)).astype(np.float64)
+        # convert to tensor
+        # image = torch.tensor(image, dtype=torch.float).cuda()
+        image = torch.tensor(image, dtype=torch.float)
+        image = image.to(device)
+        # add batch dimension
+        image = torch.unsqueeze(image, 0)
+        with torch.no_grad():
+            outputs = model(image)
+
+        # load all detection to CPU for further operations
+        outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
+        # carry further only if there are detected boxes
+        if len(outputs[0]['boxes']) != 0:
+            boxes = outputs[0]['boxes'].data.numpy()
+            scores = outputs[0]['scores'].data.numpy()
+            # filter out boxes according to `detection_threshold`
+            boxes = boxes[scores >= detection_threshold].astype(np.int32)
+            draw_boxes = boxes.copy()
+            # get all the predicited class names
+            pred_classes = [CLASSES[i] for i in outputs[0]['labels'].cpu().numpy()]
+
+            # draw the bounding boxes and write the class name on top of it
+            for j, box in enumerate(draw_boxes):
+                cv2.rectangle(orig_image,
+                              (int(box[0]), int(box[1])),
+                              (int(box[2]), int(box[3])),
+                              (0, 0, 255), 2)
+                # cv2.putText(orig_image, pred_classes[j],
+                #             (int(box[0]), int(box[1]-5)),
+                #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0),
+                #             2, lineType=cv2.LINE_AA)
+            # cv2.imshow('Prediction', orig_image)
+            # cv2.waitKey(1)
+            # cv2.imwrite(f"../test_predictions/{image_name}.jpg", orig_image,)
+            plt.imshow(orig_image)
+            plt.savefig("bounding_box_outputs/" + self.image_name + ".png")
+        print("Image {image_num} done...".format(image_num=(i + 1)))
